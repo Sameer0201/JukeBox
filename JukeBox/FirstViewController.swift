98182569562b31e5c257a5e762f34af92311a65c
TheSam2196 <sameergupta0201@gmail.com>
2017-07-19 16:08:13 +0530

    Initial Commit

---
+++ JukeBox/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  JukeBox
+//
+//  Created by Sameer on 19/07/17.
+//  Copyright © 2017 Stone. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
